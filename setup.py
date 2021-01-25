df284089e32b916467bd0ef78582a743c4d2b04d
w5tf3 <48763345+w5tf3@users.noreply.github.com>
2021-01-25 09:35:37 +0100

    change dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup
 setup(
       name='detuned_excitation',
       version='0.0.1',
-      install_requires=['numpy', 'matplotlib']
+      install_requires=[
+                        'numpy',
+                        'matplotlib',
+                        'tqdm'
+                        ]
       )
