7a6a7c8a6ae0bbb33c89adcd86e2b300c653f64a
w5tf3 <48763345+w5tf3@users.noreply.github.com>
2021-05-17 10:51:46 +0200

    update small things

--- detuned_excitation/__init__.py
+++ detuned_excitation/__init__.py
@@ -1,2 +1,2 @@
-from .amplitude_modulation.am import am_twopulse_excitation
+from .amplitude_modulation.am import am_twocolor_fortran
 from .frequency_modulation.fm import fm_pulsed_excitation, fm_rect_pulse, fm_pulsed_fortran
